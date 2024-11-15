a8d958317cf50b1589404e16050214c0a90b0608
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2024-11-15 13:22:06 -0800

    Adding functionality for azure app config and azure key vault

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Azure.Data.AppConfiguration;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Data.SqlClient;
 using MongoDB.Driver;
 using StackExchange.Redis;
 
@@ -10,15 +13,15 @@ namespace ServiceTester
         {
             if (args.Length != 2)
             {
-                Console.WriteLine("Usage: SqlConnectivityTester <database_type> <connection_string>");
-                Console.WriteLine("Database types: sql, mongo, redis");
+                Console.WriteLine("Usage: service-tester <service_type> <connection_string>");
+                Console.WriteLine("Service types: sql, mongo, redis, appconfig, keyvault");
                 return;
             }
 
-            string databaseType = args[0].ToLower();
+            string serviceType = args[0].ToLower();
             string connectionString = args[1];
 
-            switch (databaseType)
+            switch (serviceType)
             {
                 case "sql":
                     TestSqlServerConnection(connectionString);
@@ -31,10 +34,51 @@ namespace ServiceTester
                 case "redis":
                     TestRedisConnection(connectionString);
                     break;
+                
+                case "appconfig":
+                    TestAzureAppConfig(connectionString);
+                    break;
+                
+                case "keyvault":
+                    TestAzureKeyVault(connectionString);
+                    break;
 
                 default:
-                    Console.WriteLine("Unsupported database type. Please use 'sql' or 'mongo'.");
+                    Console.WriteLine("Unsupported database type " + serviceType);
                     break;
+            }
+        }
+
+        private static void TestAzureKeyVault(string connectionString)
+        {
+            try
+            {
+                DeviceCodeCredentialOptions options = new DeviceCodeCredentialOptions();
+                var credential = new DeviceCodeCredential(options);
+                var client = new SecretClient(new Uri(connectionString), credential);
+                var properties = client.GetPropertiesOfSecrets();
+                Console.WriteLine("Connection to Azure Key Vault established successfully. Found " + properties.Count() + " properties of secrets.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while connecting to Azure Key Vault: {ex.Message}");
+            }
+        }
+
+        private static void TestAzureAppConfig(string connectionString)
+        {
+            try
+            {
+                var client = new ConfigurationClient(connectionString);
+                var selector = new SettingSelector { KeyFilter = "*", LabelFilter = "*" };
+                var settings = client.GetConfigurationSettings(selector);
+
+                List<string> labels = settings.Select(s => string.IsNullOrEmpty(s.Label) ? "<EMPTY>" : s.Label).Distinct().ToList();
+                Console.WriteLine("Connection to Azure App Configuration established successfully. Found " + settings.Count() + " config settings and the following labels: " + string.Join(", ", labels));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while connecting to Azure App Configuration: {ex.Message}");
             }
         }
 
--- README.md
+++ README.md
@@ -3,4 +3,11 @@
 Build to deploy on linux with: `dotnet publish -c Release -r linux-x64 --self-contained
 `
 
-Usage: `service-tester <sql | mongo | redis> "<connection-string>"`
+Usage: `service-tester <sql | mongo | redis | appconfig | keyvault> "<connection-string>"`
+
+## Notes
+
+* Command "appconfig" = Azure App Configuration
+* Connection string should be enclosed in double-quotes
+* Key vault connection string should be in the format: "https://<keyvault-name>.vault.azure.net"
+* Key vault will prompt you to login with URL in a separate browser to Azure so that it can authenticate your user against vault
